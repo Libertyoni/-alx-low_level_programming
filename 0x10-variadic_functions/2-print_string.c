5fb167d0e4eaf90a932cc03103d3f52b89c4e3c0
Liberty Oni <libertyonii@gmail.com>
2022-04-13 15:48:00 -0400

    ALX

--- 0x10-variadic_functions/2-print_string.c
+++ 0x10-variadic_functions/2-print_string.c
@@ -1,51 +1,34 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - prints strings, followed by a new line
- * @n: number of args passed
- * @separator: string to be printed between strings
+ * print_strings - prints strings.
+ * @separator: string to be printed between the strings.
+ * @n: number of strings passed to the function.
  *
- * Return: void
+ * Return: no return.
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list valist;
 	unsigned int i;
-	va_list params;
+	char *str;
 
-	va_start(params, n);
-	if (n != 0)
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (i != n - 1)
-			{
-				if (separator != NULL)
-					printf("%s%s", make_nil(va_arg(params, char *)), separator);
-				else
-					printf("%s", make_nil(va_arg(params, char *)));
-			}
-			else
-			{
-				printf("%s", make_nil(va_arg(params, char *)));
-			}
-		}
+		str = va_arg(valist, char *);
+
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
 	}
+
 	printf("\n");
-	va_end(params);
-}
-
-/**
- * make_nil - change s to be "(nil)" if s is null
- * @s: string to be used
- *
- * Return: pointer to @s
- */
-
-char *make_nil(char *s)
-{
-	if (s == NULL)
-		s = "(nil)";
-
-	return (s);
+	va_end(valist);
 }
