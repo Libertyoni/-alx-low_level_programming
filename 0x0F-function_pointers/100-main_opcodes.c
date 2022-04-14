c7706eeda8909e1a7fd1c2634e2a3e0f2b955cc0
Liberty Oni <libertyonii@gmail.com>
2022-04-13 19:02:07 -0700

    ALX

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -20,19 +20,18 @@ int main(int argc, char *argv[])
 	}
 	
 	nbytes = atoi(argv[1]);
-
 	if (nbytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
 	for (i = 0; i < nbytes; i++)
 	{
 		printf("%02x", opc[i] & 0xFF);
 		if (i != nbytes - 1)
 			printf(" ");
 	}
-        printf("\n");
+        
+	printf("\n");
 	return (0);
 }
